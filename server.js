6cdd2d16bc776d6cd4a3f9af2614b217a1271de7
Michael Lyon <lyoncodes@gmail.com>
2018-12-13 19:44:19 -0800

    fixes ; err

--- server.js
+++ server.js
@@ -27,33 +27,67 @@ function handleError (res) {
 }
 
 // Routes
-// app.get('/location', (req, res) => {
-//   res.status(200).send('hello_world')
-//   // Check the Database for Data
-//   const SQL = 'SELECT * FROM locations WHERE search_query=$1'
-//   const values = [req.query.data]
-//   return client.query(SQL, values)
-//     .then(data => {
-//       console.log(data)
-//     })
-//     .catch(err => {
-//       console.error(err)
-//       res.send(err)
-//     })
-// })
+// check our db for requested data
+// then if we have it, send it back
+// otherwise get it from google
+// then normalize it
+// store it in our cityexplorer database
+// then send it back
+
 
 app.get('/location', getLocation)
 app.get('/movies', getMovies)
 app.get('/yelp', getYelp)
 
 // Handlers
-
 function getLocation (req, res) {
-  return searchToLatLong(req.query.data || 'lynwood')
-    .then(locationData => {
-      res.send(locationData)
+  let lookupHandler = {
+    cacheHit: (data) => {
+      console.log('Location retreived from database')
+      res.status(200).send(data.rows[0])
+    },
+    cacheMiss: (query) => {
+      return fetchLocation(query)
+        .then(result => {
+          res.send(result)
+        })
+    }
+  }
+  lookupLocation(req.query.data, lookupHandler)
+}
+function lookupLocation (query, handler) {
+  // let query = req.query.data
+  const SQL = 'SELECT * FROM locations WHERE search_query=$1'
+  const values = [query]
+  return client.query(SQL, values)
+    .then(data => {
+      if (data.rowCount) {
+        handler.cacheHit(data)
+      } else {
+        handler.cacheMiss(query)
+      }
+    })
+    .catch(err => {
+      console.error(err)
+      res.send(err)
     })
 }
+function fetchLocation (query) {
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
+  return superagent.get(URL)
+    .then(result => {
+      console.log('got it from Goog')
+
+      let location = new Location(result.body.results[0])
+      let SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude)VALUES($1, $2, $3, $4)`
+
+      return client.query(SQL, [query, location.formatted_query, location.latitude, location.longitude])
+        .then(() => {
+          return location
+        })
+    })
+}
+
 function getYelp (req, res) {
   return searchYelp(req.query.data)
     .then(yelpData => {
@@ -99,15 +133,15 @@ function Movie (movie) {
 }
 
 // Search Functions
-function searchToLatLong (query) {
-  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
-  return superagent.get(url)
-    .then(geoData => {
-      const location = new Location(geoData.body.results[0], query)
-      return location
-    })
-    .catch(err => console.error(err))
-}
+// function searchToLatLong (query) {
+//   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
+//   return superagent.get(url)
+//     .then(geoData => {
+//       const location = new Location(geoData.body.results[0], query)
+//       return location
+//     })
+//     .catch(err => console.error(err))
+// }
 
 function searchYelp (query) {
   const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${query.latitude}&longitude=${query.longitude}`
@@ -126,9 +160,40 @@ function searchMovies (query) {
       return moviesData.body.results.map(movie => new Movie(movie))
     })
 }
-// Bad Path
 
 // Listener
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`)
 })
+
+// app.get('/location', (req, res) => {
+//   let query = req.query.data
+//   // Check the Database for Data
+//   const SQL = 'SELECT * FROM locations WHERE search_query=$1'
+//   const values = [query]
+//   return client.query(SQL, values)
+//     .then(data => {
+//       if (data.rowCount) {
+//         console.log('Location retrieved from Google')
+//         res.status(200).send(data)
+//         // TODO: normalize
+//       } else {
+//         const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`
+
+//         return superagent.get(URL)
+//           .then(result => {
+//             console.log('Location retrieved from Google')
+//             let location = new Location(result.body.results[0])
+//             let SQL = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)`
+//             return client.query(SQL, [query, location.formatted_query, location.latitude, location.longitude])
+//               .then(() => {
+//                 res.status(200).send(location)
+//               })
+//           })
+//       }
+//     })
+//     .catch(err => {
+//       console.error(err)
+//       res.send(err)
+//     })
+// })
